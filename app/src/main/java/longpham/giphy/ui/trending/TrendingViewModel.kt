0c85ce568562e47375efb42954b745c8239511d0
HoangLong <pnhoanglong@gmail.com>
2018-08-11 23:32:48 +0700

    API: change how to subscribe repository livedata in modelview

--- app/src/main/java/longpham/giphy/ui/trending/TrendingViewModel.kt
+++ app/src/main/java/longpham/giphy/ui/trending/TrendingViewModel.kt
@@ -9,29 +9,32 @@ import longpham.giphy.repository.IRepository
 import longpham.giphy.util.AppConstants
 import javax.inject.Inject
 
-class TrendingViewModel @Inject constructor(private val repository: IRepository): ViewModel() {
+class TrendingViewModel @Inject constructor(private val repository: IRepository) : ViewModel() {
     private val _imagesLiveData = MutableLiveData<MutableList<GiphyImage>>()
-    val images:  LiveData<MutableList<GiphyImage>>
+    val images: LiveData<MutableList<GiphyImage>>
         get() = _imagesLiveData
 
     private val loadedImages = mutableListOf<GiphyImage>()
-    var offset = 0
 
     private var loadingImages = false
+    private var  offset = 0
     @Synchronized
-    fun loadTrendingImages(limit: Int = AppConstants.LOAD_MORE_ITEMS_COUNT){
+    fun loadTrendingImages(limit: Int = AppConstants.LOAD_MORE_ITEMS_COUNT) {
         if (loadingImages) return // only process one loading  images task
 
         loadingImages = true
-        val liveData = repository.getTrendingImages(limit = limit,  offset = offset)
-        val observer = Observer<List<GiphyImage>?> { images ->
-            if (images?.isEmpty() == false){
-                loadedImages.addAll(images)
-                _imagesLiveData.postValue(loadedImages)
+        val newImagesLiveData = repository.getTrendingImages(limit = limit, offset = offset)
+        newImagesLiveData.observeForever(object : Observer<List<GiphyImage>?> {
+            override fun onChanged(images: List<GiphyImage>?) {
+                if (images?.isEmpty() == false) {
+                    loadedImages.addAll(images)
+                    _imagesLiveData.postValue(loadedImages)
+                    loadingImages = false
+                }
+                newImagesLiveData.removeObserver(this)
                 loadingImages = false
             }
-        }
-        liveData.observeForever(observer)
+        })
         offset += AppConstants.LOAD_MORE_ITEMS_COUNT
     }
 }
